9ddc3b04ec00bf1e53485275aa2b875d8fb1856d
Artem Neliubov <artem.neliubov@yandex.ru>
2022-11-12 16:01:49 +0300

    Add Example 005 if/else

---
+++ Example005_Practise/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Enter your username: ");
+string username = Console.ReadLine();
+
+if (username.ToLower == "Masha")
+{
+    Console.Write("Hey! This is Masha!");
+}
+else
+{
+    Console.Write ("Hey, ");
+    Console.WriteLine(username);
+}
